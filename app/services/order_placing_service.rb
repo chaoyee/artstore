8f341db33cf91447ec38d5cb32c6c9e4e4db13e4
Chao-Yee Hsu <chaoyee22@gmail.com>
2016-03-29 23:26:44 +0800

    fix order_placing_service

--- app/services/order_placing_service.rb
+++ app/services/order_placing_service.rb
@@ -5,9 +5,9 @@ class OrderPlacingService
   end
 
   def place_order!
-    @order.build_item_cache_from_cart(current_cart)
-    @order.calculate_total!(current_cart)
-    cart.clean!
+    @order.build_item_cache_from_cart(@cart)
+    @order.calculate_total!(@cart)
+    @cart.clean!
     OrderMailer.notify_order_placed(@order).deliver!
   end
 end
