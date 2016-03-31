deb94c9b54c43605e61b112ffca307fead002a7f
Chao-Yee Hsu <chaoyee22@gmail.com>
2016-03-31 15:50:22 +0800

    add ATM payment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :pay2go_cc_notify
+  before_action :authenticate_user!, except: [:pay2go_cc_notify, :pay2go_atm_complete]
 
-  protect_from_forgery except: :pay2go_cc_notify
+  protect_from_forgery except: [:pay2go_cc_notify, :pay2go_atm_complete]
 
   def create
     @order = current_user.orders.build(order_params)
@@ -48,6 +48,27 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pay2go_atm_complete
+    @order = Order.find_by(token: params[:id])
+
+    json_data = JSON.parse(params["JSONData"])
+
+    if json_data["Status"] == "SUCCESS"
+
+      @order.set_payment_with!("atm")
+      @order.make_payment!
+
+      if @order.is_paid?
+        flash[:notice] = "ATM付款成功"
+        redirect_to account_orders_path
+      else
+        render text: "ATM付款失敗"
+      end
+    else
+      render text: "交易失敗"
+    end
+
+  end
 
   private
 
