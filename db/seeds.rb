c79bf3a93e9e67fe7a974a2bf1c190fe075ef43e
Chao-Yee Hsu <chaoyee22@gmail.com>
2016-03-31 14:30:13 +0800

    modify seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create!({email: "admin@test.com",password: "12345678", password_confirmation: "12345678", is_admin: true})
+
+for i in 1..5 do
+  User.create(
+    email:    "demo_user_#{i}@test.com",
+    password: "12345678",
+    password_confirmation: "12345678"
+    )
+end
